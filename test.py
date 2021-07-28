c060ea0d48eea76a0491b9b22bb7cc8215da9071
Bruno Monteiro <brunotaxi78@outlook.pt>
2021-07-28 21:19:48 +0100

    alteracoes test.py

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ class TestCalculadora(unittest.TestCase):
         res = calculadora.add(2,2)
         self.assertEqual(res,4)
     def teste_adiciona_dec(self):
-        res = calculadora.add(5.3,2)
+        res = calculadora.add('5.3',2)
         self.assertEqual(res,7.3)
     def teste_adiciona_string_int(self):
         res = calculadora.add('bruno',2)
         self.assertEqual(res,'bruno2')
     def teste_adiciona_string_dec(self):
-        res = calculadora.add('bruno',2.2)
+        res = calculadora.add('bruno','2.2')
         self.assertEqual(res,'bruno2.2')
     def teste_adiciona_string(self):
-        res = calculadora.add('bruno','monteiro')
+        res = calculadora.add('bruno',' monteiro')
         self.assertEqual(res,'bruno monteiro')
 
 if __name__ == "__main__":
